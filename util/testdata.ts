a791acf3ff6e6f5a61ed3f309557558e2a4c92f3
Dyon Vermeulen <dyon.vermeulen@bartosz.nl>
2025-01-20 09:18:09 +0100

    update testdata structure

--- util/testdata.ts
+++ util/testdata.ts
@@ -1,17 +1,44 @@
+import { BasePageMultiLanguage } from '../pages/basePageMultiLanguage';
+
 export class Testdata {
-    private _institutionAdminUsername: string;
-    private _institutionAdminPassword: string;
+  private _institutionAdminUsername: string;
+  private _institutionAdminPassword: string;
+  private _language: Language = Language.en;
+  private _pagesForLanguageChangeNotification: BasePageMultiLanguage[] = [];
 
-    constructor(institutionAdminUsername : string, institutionAdminPassword: string) {
-        this._institutionAdminUsername = institutionAdminUsername;
-        this._institutionAdminPassword = institutionAdminPassword;
-    }
+  constructor(
+    institutionAdminUsername: string,
+    institutionAdminPassword: string,
+  ) {
+    this._institutionAdminUsername = institutionAdminUsername;
+    this._institutionAdminPassword = institutionAdminPassword;
+  }
 
-    get institutionAdminUsername(): string {
-        return this._institutionAdminUsername;
+  get institutionAdminUsername(): string {
+    return this._institutionAdminUsername;
+  }
+
+  get institutionAdminPassword(): string {
+    return this._institutionAdminPassword;
+  }
+
+  get language(): Language {
+    return this._language;
+  }
+
+  set language(value: Language) {
+    this._language = value;
+    for (var page of this._pagesForLanguageChangeNotification) {
+      page.languageChange();
     }
-    
-    get institutionAdminPassword(): string {
-        return this._institutionAdminPassword;
-    }
+  }
+
+  public addPageForLanguageChangeNotification(page: BasePageMultiLanguage) {
+    this._pagesForLanguageChangeNotification.push(page);
+  }
+}
+
+export enum Language {
+  en = 'en',
+  nl = 'nl',
 }
