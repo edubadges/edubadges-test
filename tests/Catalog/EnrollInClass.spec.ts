24a710bd1788482b89ba3e7d8b3f076c437d8f23
Dyon Vermeulen <dyon.vermeulen@bartosz.nl>
2025-01-22 15:57:07 +0100

    format

--- tests/Catalog/EnrollInClass.spec.ts
+++ tests/Catalog/EnrollInClass.spec.ts
@@ -1,9 +1,6 @@
-import { expect, test } from "../../fixtures/catalogFixture";
+import { expect, test } from '../../fixtures/catalogFixture';
 
-
-test('Student can enroll in batch class', async ({
-  catalogPage, page
-}) => {
+test('Student can enroll in batch class', async ({ catalogPage, page }) => {
   await catalogPage.SearchForClass('Psychometrics');
   await catalogPage.filterOn('university-example.org');
   await catalogPage.openEduClass('Psychometrics');
