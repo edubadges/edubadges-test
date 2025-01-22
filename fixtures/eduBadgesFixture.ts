33f66a3c17141bcdffca5901fdacaf8ff54d9468
Dyon Vermeulen <dyon.vermeulen@bartosz.nl>
2025-01-22 15:24:20 +0100

    format

--- fixtures/eduBadgesFixture.ts
+++ fixtures/eduBadgesFixture.ts
@@ -12,20 +12,30 @@ var institutionAdminUsername = process.env.INSTUTUTION_ADMIN_USERNAME || '';
 var institutionAdminPassword = process.env.INSTUTUTION_ADMIN_PASSWORD || '';
 var issuerGroupAdminUsername = process.env.ISSUER_GROUP_ADMIN_USERNAME || '';
 var iissuerGroupAdminPassword = process.env.ISSUER_GROUP_ADMIN_PASSWORD || '';
-var issuerGroupAdminIssuerGroup = process.env.ISSUER_GROUP_ADMIN_ISSUERGROUP || '';
+var issuerGroupAdminIssuerGroup =
+  process.env.ISSUER_GROUP_ADMIN_ISSUERGROUP || '';
 var issuerAdminUsername = process.env.ISSUER_ADMIN_USERNAME || '';
 var iissuerAdminPassword = process.env.ISSUER_ADMIN_PASSWORD || '';
 var badgeClassAdminUsername = process.env.BADGE_CLASS_ADMIN_USERNAME || '';
 var badgeClassAdminPassword = process.env.BADGE_CLASS_ADMIN_PASSWORD || '';
 
-var testdata = new Testdata(institutionAdminUsername, institutionAdminPassword, issuerGroupAdminUsername, iissuerGroupAdminPassword, issuerGroupAdminIssuerGroup, 
-  issuerAdminUsername, iissuerAdminPassword, badgeClassAdminUsername, badgeClassAdminPassword);
+var testdata = new Testdata(
+  institutionAdminUsername,
+  institutionAdminPassword,
+  issuerGroupAdminUsername,
+  iissuerGroupAdminPassword,
+  issuerGroupAdminIssuerGroup,
+  issuerAdminUsername,
+  iissuerAdminPassword,
+  badgeClassAdminUsername,
+  badgeClassAdminPassword,
+);
 
 export const test = base.extend<EdubadgeFixture>({
   issuerPortalPage: async ({ page }, use) => {
     // Set up the fixture.
     const loginPage = new LoginPage(page, testdata);
-    await loginPage.navigateToLoginPage();
+    await loginPage.navigateToLoginPageForIssuerPortal();
     await loginPage.loginWithInstitutionAdmin();
     const issuerPortalPage = new IssuerPortalPage(page, testdata);
     await issuerPortalPage.goToManage();
