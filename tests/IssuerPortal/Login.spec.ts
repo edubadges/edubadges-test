f7fca1ec1250e29c8435f1a9a7b0a1c306faf646
Dyon Vermeulen <dyon.vermeulen@bartosz.nl>
2025-01-22 15:22:36 +0100

    rename test cases + student login test case

--- tests/IssuerPortal/Login.spec.ts
+++ tests/IssuerPortal/Login.spec.ts
@@ -1,29 +1,51 @@
 import { test } from '../../fixtures/loginFixture';
 
-test('Login with institution admin', async ({ loginPage, issuerPortalPage }) => {
-  await loginPage.navigateToLoginPage();
+test('Login with institution admin in the issuer portal', async ({
+  loginPage,
+  issuerPortalPage,
+}) => {
+  await loginPage.navigateToLoginPageForIssuerPortal();
   await loginPage.expectLoginPageOpened();
   await loginPage.loginWithInstitutionAdmin();
   await issuerPortalPage.validateLoginSuccesfull();
 });
 
-test('Login with issuer group admin', async ({ loginPage, issuerPortalPage }) => {
-  await loginPage.navigateToLoginPage();
+test('Login with issuer group admin in the issuer portal', async ({
+  loginPage,
+  issuerPortalPage,
+}) => {
+  await loginPage.navigateToLoginPageForIssuerPortal();
   await loginPage.expectLoginPageOpened();
   await loginPage.loginWithIssuerGroupAdmin();
   await issuerPortalPage.validateLoginSuccesfull();
 });
 
-test('Login with badge class admin', async ({ loginPage, issuerPortalPage }) => {
-  await loginPage.navigateToLoginPage();
+test('Login with badge class admin in the issuer portal', async ({
+  loginPage,
+  issuerPortalPage,
+}) => {
+  await loginPage.navigateToLoginPageForIssuerPortal();
   await loginPage.expectLoginPageOpened();
   await loginPage.loginWithBadgeClassAdmin();
   await issuerPortalPage.validateLoginSuccesfull();
 });
 
-test('Login with issuerAdmin', async ({ loginPage, issuerPortalPage }) => {
-  await loginPage.navigateToLoginPage();
+test('Login with issuerAdmin in the issuer portal', async ({
+  loginPage,
+  issuerPortalPage,
+}) => {
+  await loginPage.navigateToLoginPageForIssuerPortal();
   await loginPage.expectLoginPageOpened();
   await loginPage.loginWithIssuerAdmin();
   await issuerPortalPage.validateLoginSuccesfull();
+});
+
+test('Login with student in the issuer portal', async ({
+  loginPage,
+  issuerPortalPage,
+}) => {
+  await loginPage.navigateToLoginPageForIssuerPortal();
+  await loginPage.expectLoginPageOpened();
+  await loginPage.loginWithStudent();
+  await issuerPortalPage.validateLoginFailed();
 });
