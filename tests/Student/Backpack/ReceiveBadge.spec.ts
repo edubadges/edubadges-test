37fbcaab236c2950cc806a7ae7e8b4205d5ca847
Max van Pelt <55793121+ikbenwasmachine@users.noreply.github.com>
2025-03-11 09:19:30 +0100

    Rename RemoveBadge
    To be able to add other tests that fall under the "Badge has been received"

---
+++ tests/Student/Backpack/ReceiveBadge.spec.ts
@@ -0,0 +1,41 @@
+import { expect, test } from '../../../fixtures/catalogFixture';
+
+test('Reject received badge', async ({
+    backpackPage,
+    issuerPortalPage,
+}) => {
+    // var
+    const course = "Law and Politics";
+
+    // setup
+    await issuerPortalPage.directAwardBadgeToStudent(course);
+    
+    // test
+    await backpackPage.OpenBackpack();
+    await backpackPage.page.getByText('View details to claim this edubadge').click();
+    await backpackPage.page.waitForTimeout(500);
+    await backpackPage.page.getByRole('link', { name: 'Reject' }).click();
+    await backpackPage.page.waitForTimeout(500);
+    await backpackPage.page.getByRole('link', {name: 'Confirm'}).click();
+    await expect(backpackPage.page.getByText('Edubadge is rejected')).toBeVisible();
+});
+
+test('Accept received badge', async ({
+    backpackPage,
+    issuerPortalPage,
+}) => {
+    // var
+    const course = "Law and Politics";
+
+    // setup
+    await issuerPortalPage.directAwardBadgeToStudent(course);
+    
+    // test
+    await backpackPage.OpenBackpack();
+    await backpackPage.page.getByText('View details to claim this edubadge').click();
+    await backpackPage.page.waitForTimeout(500);
+    await backpackPage.page.getByRole('link', { name: 'Accept' }).click();
+    await backpackPage.page.waitForTimeout(500);
+    await backpackPage.page.getByRole('link', {name: 'Confirm'}).click();
+    await expect(backpackPage.page.getByText('Edubadge is accepted')).toBeVisible();
+});
