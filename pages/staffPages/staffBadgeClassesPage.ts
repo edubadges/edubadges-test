8b0bf82a22a99c080c1feef18699d7a94de75040
Max van Pelt <55793121+ikbenwasmachine@users.noreply.github.com>
2025-03-20 13:43:15 +0100

    Documentation

--- pages/staffPages/staffBadgeClassesPage.ts
+++ pages/staffPages/staffBadgeClassesPage.ts
@@ -20,6 +20,7 @@ export class staffBadgeClassesPage extends BaseStaffSubPage {
     await this.page.getByRole('link', { name: 'Award' }).click();
   }
 
+  /**Opens a badge by name if it is currently on screen. */
   async openBadge(badgeName: string){
     await this.page.locator('.content')
       .getByText(badgeName)
