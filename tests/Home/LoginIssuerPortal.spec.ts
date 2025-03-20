48a2f40184678a456e057ebb448c4f7ffbb9558f
Max van Pelt <55793121+ikbenwasmachine@users.noreply.github.com>
2025-03-20 10:49:35 +0100

    Remove duplicate tests
    tests are replaced by the {user}/basics tests

--- tests/Home/LoginIssuerPortal.spec.ts
+++ tests/Home/LoginIssuerPortal.spec.ts
@@ -1,58 +1,5 @@
 import { test } from '../../fixtures/loginFixture';
 
-// TODO: create basicsLogin test
-test('Login with institution admin for uni in the issuer portal', async ({
-  homePage,
-  issuerPortalPage,
-}) => {
-  await homePage.expectHomePageOpened();
-  await homePage.openIssuerPortal();
-  await issuerPortalPage.loginWithWoInstitutionAdmin();
-  await issuerPortalPage.validateLoginSuccesfull();
-});
-
-test('Login with issuer group admin for uni in the issuer portal', async ({
-  homePage,
-  issuerPortalPage,
-}) => {
-  await homePage.expectHomePageOpened();
-  await homePage.openIssuerPortal();
-  await issuerPortalPage.loginWithWoIssuerGroupAdmin();
-  await issuerPortalPage.validateLoginSuccesfull();
-});
-
-test('Login with badge class admin for uni in the issuer portal', async ({
-  homePage,
-  issuerPortalPage,
-}) => {
-  await homePage.expectHomePageOpened();
-  await homePage.openIssuerPortal();
-  await issuerPortalPage.loginWithWoBadgeClassAdmin();
-  await issuerPortalPage.validateLoginSuccesfull();
-});
-
-test('Login with issuerAdmin for uni in the issuer portal', async ({
-  homePage,
-  issuerPortalPage,
-}) => {
-  await homePage.expectHomePageOpened();
-  await homePage.openIssuerPortal();
-  await issuerPortalPage.loginWithWoIssuerAdmin();
-  await issuerPortalPage.validateLoginSuccesfull();
-});
-
-/* Replaced by Student -> Backpack -> BasicsBackpack
-test('Login with student in the issuer portal', async ({
-  homePage,
-  issuerPortalPage,
-}) => {
-  await homePage.expectHomePageOpened();
-  await homePage.openIssuerPortal();
-  await issuerPortalPage.loginWithStudent();
-  await issuerPortalPage.validateLoginFailed();
-});
-*/
-
 test('Login with institution admin for MBO in the issuer portal', async ({
   homePage,
   issuerPortalPage,
