49b69f09f1ab02a0cb7744d5088ed957c0568eba
Max van Pelt <55793121+ikbenwasmachine@users.noreply.github.com>
2025-03-21 11:26:48 +0100

    Improved createIssuerGroup function

--- pages/staffPages/staffManageSubPage/issuerGroupsSubPage.ts
+++ pages/staffPages/staffManageSubPage/issuerGroupsSubPage.ts
@@ -3,11 +3,17 @@ import { BaseStaffSubPage } from '../baseStaffSubPage';
 import path from 'path';
 
 export class IssuerGroupSubPage extends BaseStaffSubPage {
-    // var
-    async AddNewIssuerGroup(nameGroup: string){
+    async AddNewIssuerGroup(
+            groupName: string,
+            groupDesc: string = 'Test issuer group description',
+            isEnglish: boolean = false,
+            issueOnBehalf: boolean= false,
+            organisationURL: string = '',
+            linkedInURL: string = '',){
         await this.page.getByRole('link', { name: 'Add new issuer group'}).click();
         await this.waitForLoadingToStop();
 
+        await this.FillIssuerGroupForm(groupName, groupDesc, isEnglish, issueOnBehalf, organisationURL, linkedInURL);
         await this.page.getByRole('link', { name: 'Save' }).click();
         
         // validate
@@ -16,30 +22,37 @@ export class IssuerGroupSubPage extends BaseStaffSubPage {
 
     private async FillIssuerGroupForm(
         groupName: string,
-        groupDesc: string = 'Test issuer group description',
-        isEnglish: boolean = false,
-        issueOnBehalf: boolean= false,
-        organisationURL: string = '',
-        linkedInURL: string = '',
+        groupDesc: string,
+        isEnglish: boolean,
+        issueOnBehalf: boolean,
+        organisationURL: string,
+        linkedInURL: string,
     ){
         //var
         const titleField = this.page.getByPlaceholder('(Required field) e.g. History');
         const descField = this.page.getByPlaceholder('(Required field) e.g. all History related studies');
         const issueButton = this.page.locator('.slider');
-        const fileChooser = await this.page.waitForEvent('filechooser');
+        const uploadButton = this.page.getByText('Upload image');
         const virtOrgField = this.page.getByPlaceholder('(Optional) e.g. URL of the other organisation');
         const linkedInField = this.page.getByPlaceholder('(Optional) e.g. the linkedin identifier of the other organisation');
 
         // fill
-        await this.page.getByRole('link', { name: isEnglish? 'English' : 'Dutch' }).click();
+        await this.page
+            .locator('.tab-choice')
+            .getByText( isEnglish? 'English' : 'Dutch')
+            .click();
         await titleField.fill(groupName);
         await descField.fill(groupDesc);
 
         if(issueOnBehalf){ 
-            issueButton.click({ force: true });
+            await issueButton.click({ force: true });
+            await expect(uploadButton).toHaveAttribute('disabled', 'false');
+
+            const fileChooserPromise = this.page.waitForEvent('filechooser');
             await this.page.getByText('Upload image').click();
+            const fileChooser = await fileChooserPromise;
             await fileChooser.setFiles(
-            path.join(__dirname + '../images/', 'edubadge.png'),
+              path.join(__dirname + '../../images/', 'edubadge.png'),
             );
             virtOrgField.fill(organisationURL);
             linkedInField.fill(linkedInURL);
