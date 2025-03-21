811dbc2cf5562aecd30ee9038bc37db621aa7e2c
Max van Pelt <55793121+ikbenwasmachine@users.noreply.github.com>
2025-03-21 14:50:20 +0100

    initial delete issuer group test

---
+++ tests/InstitutionAdmin/EntityControl/Delete/DeleteWOIssuerGroup.spec.ts
@@ -0,0 +1,26 @@
+import { expect, test } from '../../../../fixtures/staffFixtures/staffWOFixture';
+
+test('Delete WO issuer group', async ({
+    woPage,
+  }) => {
+    // var
+    const issuerGroupName = "GroupToRemove";
+    const newIssuerGroupDesc = "This group was made to be removed";
+    const issuerGroup = woPage.managePage.issuerGroupPage;
+
+    // setup
+    await woPage.goToManage();
+    await woPage.managePage.goToIssuerGroups();
+    await issuerGroup.AddNewIssuerGroup(issuerGroupName, newIssuerGroupDesc);
+    await woPage.goToManage();
+    await woPage.managePage.goToIssuerGroups();
+    await woPage.waitForLoadingToStop();
+
+    // test
+    await woPage.goToManage();
+    await woPage.managePage.goToIssuerGroups();
+    await issuerGroup.DeleteExistingIssuerGroup(newIssuerGroupDesc)
+
+    // validate
+    await expect(woPage.page.getByText('Successfully deleted issuer group')).toBeVisible();
+  });
