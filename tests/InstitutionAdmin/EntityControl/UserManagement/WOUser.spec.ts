09334e32a356c850d1e28f83f85e255694cd317b
Max van Pelt <55793121+ikbenwasmachine@users.noreply.github.com>
2025-03-24 14:00:33 +0100

    Initial user management test

---
+++ tests/InstitutionAdmin/EntityControl/UserManagement/WOUser.spec.ts
@@ -0,0 +1,39 @@
+import { expect, test } from '../../../../fixtures/staffFixtures/staffWOFixture';
+
+test('Invite WO user', async ({
+    woPage,
+  }) => {
+    // var
+    const newUserMail = "FirstTestMailAdress@testMailAdress.test";
+    const userManagement = woPage.managePage.userManagePage;
+
+    // setup
+    await woPage.goToManage();
+    await woPage.managePage.goToUserManagement();
+
+    // test
+    await userManagement.addNewUser(newUserMail);
+
+    // validate
+    await expect(woPage.page.getByText(`Successfully invited ${newUserMail}`)).toBeVisible();
+  });
+
+  test('Revoke WO user invite', async ({
+    woPage,
+  }) => {
+    // var
+    const newUserMail = "SecondTestMailAre@testMailAdress.test";
+    const userManagement = woPage.managePage.userManagePage;
+
+    // setup
+    await woPage.goToManage();
+    await woPage.managePage.goToUserManagement();
+    await userManagement.addNewUser(newUserMail);
+
+    // test
+    await userManagement.removeExistingPermissions(newUserMail);
+
+    // validate
+    await expect(woPage.page.getByText('Successfully removed invite')).toBeVisible();
+    await expect(woPage.page.getByText(newUserMail)).not.toBeVisible();
+  });
