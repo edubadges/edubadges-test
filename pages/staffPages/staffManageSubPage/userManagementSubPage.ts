2a1ccd5d1fbb258f6b3544cf468165d4b426e3aa
Max van Pelt <55793121+ikbenwasmachine@users.noreply.github.com>
2025-03-24 11:52:22 +0100

    Initial user management subpage

--- pages/staffPages/staffManageSubPage/userManagementSubPage.ts
+++ pages/staffPages/staffManageSubPage/userManagementSubPage.ts
@@ -1,3 +1,38 @@
 import { BaseStaffSubPage } from "../baseStaffSubPage";
 
-export class UserManagementSubPage extends BaseStaffSubPage {}
+export class UserManagementSubPage extends BaseStaffSubPage {
+
+    async addNewUser(emailAdress: string, role: string = "Institution admin"
+    ){
+        await this.fillNewUserForm(emailAdress, role);
+        await this.page.getByRole('link', { name: "Add"}).click();
+        await this.page.getByText(`Successfully invited ${emailAdress}`).waitFor();
+    }
+
+    async fillNewUserForm(emailAdress: string,
+        role: string,
+        inputNumber: number = 0 ){
+            const emailAdressForm = this.page.getByPlaceholder("e.g. john.doe@example.com").nth(inputNumber);
+            
+            await emailAdressForm.fill(emailAdress);
+
+            if(role != "Institution admin"){
+                // WHEN DOES THIS HAPPEN
+            }
+    }
+
+    async removeExistingPermissions(nameOrEmail: string){
+        const rowLocator = this.page.getByText(nameOrEmail).locator('../..');
+        const removeButton = this.page.getByText('Remove permissions');
+
+        await rowLocator.locator('.checkboxed').click();
+        await removeButton.waitFor();
+        await removeButton.click();
+        await this.page.getByRole('link', { name: 'Confirm' }).click();
+        
+        // validate
+        await this.page.getByText('Successfully removed invite')
+            .or(this.page.getByText('Successfully removed rights'))
+            .waitFor();
+    }
+}
