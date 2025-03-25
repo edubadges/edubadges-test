b087d048e115773a4c4a96206362fba478b47495
Max van Pelt <55793121+ikbenwasmachine@users.noreply.github.com>
2025-03-25 16:11:52 +0100

    Initial editing of badge test + screenshot

--- tests/Teacher/BadgeInteraction/BadgeEditing/WoEditBadge.spec.ts
+++ tests/Teacher/BadgeInteraction/BadgeEditing/WoEditBadge.spec.ts
@@ -3,37 +3,36 @@ import { expect, test } from '../../../../fixtures/staffFixtures/staffWOFixture'
 
 test('Edit existing edubadge', async ({
     woPage,
-    testdata,
   }) => {
     // var
+    const issuerGroupName = "Medicine";
+    const initialBadgeName = "3FirstNameEditBadge";
+    const editedBadgeName = "SecondNameEditBadge";
+    const editedBadgeDesc = "newDescription";
+    const editedBadgeOutcome = "The new outcome";
+    const editedBadgeCriterium = "The new criterium";
+    const editedBadgeEQF = "EQF 7";
+    const editedBadgeIdentifier = "1928";
+    const editedBadgeFormOfPart = "Blended";
+    const editedBadgeAssesment = "Behavioural assessment";
     const issuers = woPage.managePage.issuersPage;
   
     // setup
-    testdata.badgeData.title = 'Extra curricular badge';
     await woPage.goToManage();
-    await issuers.searchWithText('Medicine');
-    await issuers.openIssuerGroup('Medicine');
-    await issuers.clickNewBadgeClass();
-    await issuers.clickExtraCurricularEduBadge();
+    await issuers.createRegularBadge(issuerGroupName, initialBadgeName);
   
     // test
-    await issuers.fillInExtraCurricularForm();
-    await issuers.publishBadge();
-  
-    await woPage.page
-      .getByRole('link', { name: 'Edit badge class' })
-      .waitFor();
-      
-    var maskedLocators = [
-      woPage.page
-        .getByText('Created ')
-        .locator('..')
-        .locator('..'),
-    ];
-    await expect(woPage.page).toHaveScreenshot(
-      'extraCurricularBadgeCreated.png',
-      {
-        mask: maskedLocators,
-      },
+    await issuers.editExistingBadge(editedBadgeName, editedBadgeDesc,
+        editedBadgeOutcome, editedBadgeCriterium,
+        editedBadgeEQF, editedBadgeIdentifier,
+        editedBadgeFormOfPart, editedBadgeAssesment,
     );
+    
+    // validation
+  let maskedLocators = [
+    woPage.page
+      .getByText('Created ')
+      .locator('..'),
+  ];
+    await expect(woPage.page).toHaveScreenshot('EditedWOBadgeclass.png', { fullPage: true, mask: maskedLocators });
   });
