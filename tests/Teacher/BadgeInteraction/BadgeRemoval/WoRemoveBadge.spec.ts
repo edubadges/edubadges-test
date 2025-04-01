43ff8a38a75a06f79e261d0df45d287acdcc8122
Max van Pelt <55793121+ikbenwasmachine@users.noreply.github.com>
2025-04-01 13:28:37 +0200

    Expand badge removal test
    added micro credential and extra curricular removal

--- tests/Teacher/BadgeInteraction/BadgeRemoval/WoRemoveBadge.spec.ts
+++ tests/Teacher/BadgeInteraction/BadgeRemoval/WoRemoveBadge.spec.ts
@@ -1,17 +1,57 @@
 import { expect, test } from '../../../../fixtures/staffFixtures/WOFixtures/InstitutionAdminWOFixture';
 
 
-test('Remove existing edubadge', async ({
+test('Remove existing regular WO badge', async ({
     woInstitutionAdminPage: woPage,
   }) => {
     // var
     const issuerGroupName = "Medicine";
-    const badgeName = "BadgeName to delete, should be deleted";
+    const badgeName = "Regular badge to delete, should be deleted";
     const issuers = woPage.managePage.issuersPage;
   
     // setup
     await woPage.goToManage();
     await issuers.createRegularBadge(issuerGroupName, badgeName);
+  
+    // test
+    await issuers.removeExistingBadge();
+
+    // validation
+    await issuers.searchWithText(badgeName);
+    await expect(woPage.page.locator('td')).toHaveCount(0);
+  });
+
+test('Remove existing WO micro credential', async ({
+    woInstitutionAdminPage: woPage,
+  }) => {
+    // var
+    const issuerGroupName = "Medicine";
+    const badgeName = "Micro credential badge to delete, should be deleted";
+    const issuers = woPage.managePage.issuersPage;
+  
+    // setup
+    await woPage.goToManage();
+    await issuers.createMicroBadge(issuerGroupName, badgeName);
+  
+    // test
+    await issuers.removeExistingBadge();
+
+    // validation
+    await issuers.searchWithText(badgeName);
+    await expect(woPage.page.locator('td')).toHaveCount(0);
+  });
+
+test('Remove existing extra curricular WO badge', async ({
+    woInstitutionAdminPage: woPage,
+  }) => {
+    // var
+    const issuerGroupName = "Medicine";
+    const badgeName = "Extra curricular badge to delete, should be deleted";
+    const issuers = woPage.managePage.issuersPage;
+  
+    // setup
+    await woPage.goToManage();
+    await issuers.createExtracurricularBadge(issuerGroupName, badgeName);
   
     // test
     await issuers.removeExistingBadge();
