83cc1667fcc7e37dc78517d09c6e69f85a02aac8
Max van Pelt <55793121+ikbenwasmachine@users.noreply.github.com>
2025-04-02 14:26:06 +0200

    Update reading of existing issuer
    added snapshot and used correct open method

--- tests/InstitutionAdmin/EntityControl/Issuers/Read/ReadWOIssuer.spec.ts
+++ tests/InstitutionAdmin/EntityControl/Issuers/Read/ReadWOIssuer.spec.ts
@@ -4,14 +4,14 @@ test('See WO issuer', async ({
     woInstitutionAdminPage: woPage,
   }) => {
     // var
-    const existingGroupName = "Science";
+    const existingGroupName = "Computer Science";
     const issuer = woPage.managePage.issuersPage;
     const maskBadgeClass = woPage.page.locator('th').getByText('Badge Classes');
     const maskDate = woPage.page.getByText('Created ').locator('../..');
 
     // test
     await woPage.goToManage();
-    await woPage.managePage.goToIssuerGroups();
+    await woPage.managePage.goToIssuers();
     await issuer.openIssuer(existingGroupName);
     await woPage.waitForLoadingToStop();
 
