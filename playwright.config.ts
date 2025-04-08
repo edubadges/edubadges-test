c9e8cc80009cbe4a173ba66d85c0401341732b93
Max van Pelt <55793121+ikbenwasmachine@users.noreply.github.com>
2025-04-08 12:48:41 +0200

    Made tests run in parallel on local
    chose 2 because more parallelism can cause flakyness

--- playwright.config.ts
+++ playwright.config.ts
@@ -18,7 +18,7 @@ export default defineConfig({
   retries: process.env.CI ? 2 : 1,
 
   // Opt out of parallel tests on CI.
-  workers: process.env.CI ? 1 : 1,
+  workers: process.env.CI ? 1 : 2,
 
   // Reporter to use
   reporter: [
