7ca2aa967261c8e03f87e730d385d08952ae5d23
Max van Pelt <55793121+ikbenwasmachine@users.noreply.github.com>
2025-04-10 18:31:13 +0200

    Update AwardBadge to follow new standard

--- tests/Teacher/StudentInteraction/AwardBadge.spec.ts
+++ tests/Teacher/StudentInteraction/AwardBadge.spec.ts
@@ -1,35 +1,39 @@
-import { expect, test } from '../../../fixtures/staffFixtures/WOFixtures/InstitutionAdminWOFixture';
-// TODO: add parameterised test to also test with other accounts.
-// TODO: bulk award
+import { expect, test } from '../../../fixtures/staffFixture';
+import { institutions } from '../../../util/loginPossibilities';
 
-test('Award requested badge', async ({
+institutions.forEach(institution => {
+test(`Award requested badge from ${institution}`, async ({
   catalogPage,
-  woInstitutionAdminPage: woPage,
+  adminPage,
 }) => {
   // var
   const course = 'Psychometrics';
-  const institution = 'university-example.org';
 
   // setup
+  await adminPage.loginTestIdp(institution, 'Institution');
   await catalogPage.searchForClass(course);
   await catalogPage.filterOn(institution);
   await catalogPage.openEduClass(course);
   await catalogPage.requestEdubadge();
 
   // test
-  await woPage.badgeClassPage.approveRequest(course);
+  await adminPage.badgeClassPage.approveRequest(course);
 
   // validate
-  await expect(woPage.page.getByText('The request(s) have been awarded.')).toBeVisible();
+  await expect(adminPage.page.getByText('The request(s) have been awarded.')).toBeVisible();
 });
 
-test('Send badge directly', async ({ woInstitutionAdminPage: woPage }) => {
+test(`Send badge directly from ${institution}`, async ({ 
+  adminPage 
+}) => {
   // var
   const courseName = 'Cognitive Psychology';
 
   // test
-  await woPage.badgeClassPage.directAwardBadgeToStudent(courseName);
+  await adminPage.loginTestIdp(institution, 'Institution');
+  await adminPage.badgeClassPage.directAwardBadgeToStudent(courseName);
 
   // validate
-  await expect(woPage.page.getByText('Direct awards have been sent')).toBeVisible();
+  await expect(adminPage.page.getByText('Direct awards have been sent')).toBeVisible();
+});
 });
