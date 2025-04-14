a15a8a3aa1072ba5d4d32c3575712340ef013d36
Max van Pelt <55793121+ikbenwasmachine@users.noreply.github.com>
2025-04-14 18:15:33 +0200

    Update WOUser to new standard

--- tests/InstitutionAdmin/EntityControl/UserManagement/WOUser.spec.ts
+++ tests/InstitutionAdmin/EntityControl/UserManagement/WOUser.spec.ts
@@ -1,31 +1,36 @@
-import { expect, test } from '../../../../fixtures/staffFixtures/WOFixtures/InstitutionAdminWOFixture';
+import { expect, test } from '../../../../fixtures/staffFixture';
+import { institutions } from '..//../../../util/loginPossibilities';
 
-test('Invite WO user', async ({ woInstitutionAdminPage: woPage }) => {
+institutions.forEach(institution => {
+
+test(`Invite ${institution} user`, async ({ adminPage }) => {
   // var
-  const newUserMail = 'TestNewFirstTestMailAdress@university.org';
-  const userManagement = woPage.managePage.userManagePage;
+  const userManagement = adminPage.managePage.userManagePage;
+  const newUserMail = `userToInvite@${institution}mail.edu`;
 
   // setup
-  await woPage.goToManage();
-  await woPage.managePage.goToUserManagement();
+  await adminPage.loginTestIdp(institution, 'Institution');
+  await adminPage.goToManage();
+  await adminPage.managePage.goToUserManagement();
 
   // test
   await userManagement.addNewUser(newUserMail);
 
   // validate
   await expect(
-    woPage.page.getByText(`Successfully invited ${newUserMail}`),
+    adminPage.page.getByText(`Successfully invited ${newUserMail}`),
   ).toBeVisible();
 });
 
-test('Revoke WO user invite', async ({ woInstitutionAdminPage: woPage }) => {
+test(`Revoke ${institution} user invite`, async ({ adminPage }) => {
   // var
-  const newUserMail = 'SecondTestMailAdress@university.org';
-  const userManagement = woPage.managePage.userManagePage;
+  const userManagement = adminPage.managePage.userManagePage;
+  const newUserMail = `userToRevoke@${institution}mail.edu`;
 
   // setup
-  await woPage.goToManage();
-  await woPage.managePage.goToUserManagement();
+  await adminPage.loginTestIdp(institution, 'Institution');
+  await adminPage.goToManage();
+  await adminPage.managePage.goToUserManagement();
   await userManagement.addNewUser(newUserMail);
 
   // test
@@ -33,54 +38,59 @@ test('Revoke WO user invite', async ({ woInstitutionAdminPage: woPage }) => {
 
   // validate
   await expect(
-    woPage.page.getByText('Successfully removed invite'),
+    adminPage.page.getByText('Successfully removed invite'),
   ).toBeVisible();
-  await expect(woPage.page.getByText(newUserMail)).not.toBeVisible();
+  await expect(adminPage.page.getByText(newUserMail)).not.toBeVisible();
 });
 
-test('Accept WO invite', async ({
-  woInstitutionAdminPage: woPage,
-  newStaffLoginPage,
+test(`Accept ${institution} invite`, async ({
+  adminPage,
+  extraStaffLoginPage,
 }) => {
   // var
-  const newUsername = 'AcceptInviteInstitutionAdmin';
-  const newUserMail = 'ThirdTestMailAdress@university.org';
-  const userManagement = woPage.managePage.userManagePage;
+  const userManagement = adminPage.managePage.userManagePage;
+  const newUsername = `Accept${institution}InviteInstitutionAdmin`;
+  const institutionServer = await userManagement.getInstitutionServer(institution);
+  const newUserMail = newUsername + '@' + institutionServer;
 
   // setup
-  await woPage.goToManage();
-  await woPage.managePage.goToUserManagement();
+  await adminPage.loginTestIdp(institution, 'Institution');
+  await adminPage.goToManage();
+  await adminPage.managePage.goToUserManagement();
 
   // test
   await userManagement.addNewUser(newUserMail);
-  await newStaffLoginPage.loginDummyIdp(newUsername, newUserMail);
+  await extraStaffLoginPage.loginDummyIdp(newUsername, newUserMail, institutionServer);
 
   // validate
-  await expect(newStaffLoginPage.page.locator('.expand-menu')).toBeVisible();
+  await expect(extraStaffLoginPage.page.locator('.expand-menu')).toBeVisible();
 });
 
-test('Delete WO permission', async ({
-  woInstitutionAdminPage: woPage,
-  newStaffLoginPage,
+test(`Delete ${institution} permission`, async ({
+  adminPage,
+  extraStaffLoginPage,
 }) => {
   // var
-  const newUsername = 'GetRemovedRightsInstitutionAdmin';
-  const newUserMail = 'FourthTestMailAdress@university.org';
-  const userManagement = woPage.managePage.userManagePage;
+  const userManagement = adminPage.managePage.userManagePage;
+  const newUsername = `GetRightsRemoved${institution}InstitutionAdmin`;
+  const institutionServer = await userManagement.getInstitutionServer(institution);
+  const newUserMail = newUsername + '@' + institutionServer;
 
   // setup
-  await woPage.goToManage();
-  await woPage.managePage.goToUserManagement();
+  await adminPage.loginTestIdp(institution, 'Institution');
+  await adminPage.goToManage();
+  await adminPage.managePage.goToUserManagement();
   await userManagement.addNewUser(newUserMail);
-  await newStaffLoginPage.loginDummyIdp(newUsername, newUserMail);
+  await extraStaffLoginPage.loginDummyIdp(newUsername, newUserMail, institutionServer);
 
   // test
-  await woPage.reloadPage();
+  await adminPage.reloadPage();
   await userManagement.removeExistingPermissions(newUserMail);
 
   // validate
   await expect(
-    woPage.page.getByText('Successfully removed rights'),
+    adminPage.page.getByText('Successfully removed rights'),
   ).toBeVisible();
-  await expect(woPage.page.getByText(newUsername)).not.toBeVisible();
+  await expect(adminPage.page.getByText(newUsername)).not.toBeVisible();
+});
 });
