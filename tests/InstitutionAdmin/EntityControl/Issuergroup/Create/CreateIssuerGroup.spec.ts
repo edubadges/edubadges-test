4ae9b0ff165f46cfe2c4eb597feacb5fbfa7dfab
Max van Pelt <55793121+ikbenwasmachine@users.noreply.github.com>
2025-04-15 09:50:29 +0200

    Update issuergroup tests to follow new standard

---
+++ tests/InstitutionAdmin/EntityControl/Issuergroup/Create/CreateIssuerGroup.spec.ts
@@ -0,0 +1,32 @@
+import { expect, test } from '../../../../../fixtures/staffFixture';
+import { institutions } from '../../../../../util/loginPossibilities';
+
+institutions.forEach(institution => {
+test(`Create new ${institution} issuer group`, async ({
+  adminPage,
+}) => {
+  // var
+  const issuerGroup = adminPage.managePage.issuerGroupPage;
+  const newIssuerGroupName = 'NewWOIssuerGroup';
+  const descriptionText = 'Test WO Issuergroup';
+
+  // setup
+  await adminPage.loginTestIdp(institution, 'Institution');
+  await adminPage.goToManage();
+  await adminPage.managePage.goToIssuerGroups();
+
+  // test
+  await issuerGroup.addNewIssuerGroup(newIssuerGroupName, descriptionText);
+
+  // validate
+  const editButton = adminPage.page.getByRole('link', {
+    name: 'Edit issuer group',
+  });
+  const groupTitle = adminPage.page.locator('.title').getByRole('heading');
+  const description = adminPage.page.locator('.info').locator('p').first();
+
+  await expect(editButton).toBeVisible();
+  await expect(groupTitle).toHaveText(newIssuerGroupName);
+  await expect(description).toHaveText(descriptionText);
+});
+});
---
+++ tests/InstitutionAdmin/EntityControl/Issuergroup/Delete/DeleteIssuerGroup.spec.ts
@@ -0,0 +1,28 @@
+import { expect, test } from '../../../../../fixtures/staffFixture';
+import { institutions } from '../../../../../util/loginPossibilities';
+
+institutions.forEach(institution => {
+test(`Delete ${institution} issuer group`, async ({ adminPage }) => {
+  // var
+  const issuerGroup = adminPage.managePage.issuerGroupPage;
+  const issuerGroupName = 'GroupToRemove';
+  const newIssuerGroupDesc = 'This group was made to be removed';
+
+  // setup
+  await adminPage.loginTestIdp(institution, 'Institution');
+  await adminPage.goToManage();
+  await adminPage.managePage.goToIssuerGroups();
+  await issuerGroup.addNewIssuerGroup(issuerGroupName, newIssuerGroupDesc);
+  await adminPage.goToManage();
+  await adminPage.managePage.goToIssuerGroups();
+  await adminPage.waitForLoadingToStop();
+
+  // test
+  await issuerGroup.deleteExistingIssuerGroup(issuerGroupName);
+
+  // validate
+  await expect(
+    adminPage.page.getByText('Successfully deleted issuer group'),
+  ).toBeVisible();
+});
+});
