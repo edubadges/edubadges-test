d7c0b32d3de18b4cdf41aa1474107460e9138048
Max van Pelt <55793121+ikbenwasmachine@users.noreply.github.com>
2025-04-15 09:36:07 +0200

    Update Issuers tests to follow new standard

---
+++ tests/InstitutionAdmin/EntityControl/Issuers/Update/UpdateIssuer.spec.ts
@@ -0,0 +1,40 @@
+import { expect, test } from '../../../../../fixtures/staffFixture';
+import { institutions } from '../../../../../util/loginPossibilities';
+
+institutions.forEach(institution => {
+test(`Edit ${institution} issuer`, async ({ adminPage }) => {
+  // var
+  const initialIssuerName = 'Initial issuer name';
+  const initialIssuerDesc = 'Initial description';
+  const editedIssuerName = 'Edited issuer name';
+  const editedIssuerDesc = 'Second description';
+
+  const issuers = adminPage.managePage.issuersPage;
+  const editButton = adminPage.page.getByRole('link', { name: 'Edit issuer' });
+
+  // setup
+  await adminPage.loginTestIdp(institution, 'Institution');
+  await adminPage.goToManage();
+  await adminPage.managePage.goToIssuers();
+  await issuers.createNewIssuer(initialIssuerName, initialIssuerDesc);
+
+  await adminPage.goToManage();
+  await adminPage.managePage.goToIssuers();
+  await adminPage.waitForLoadingToStop();
+
+  // test
+  await issuers.editExistingIssuer(
+    initialIssuerName,
+    editedIssuerName,
+    editedIssuerDesc,
+  );
+
+  // validate
+  const groupTitle = adminPage.page.locator('.title').getByRole('heading');
+  const description = adminPage.page.locator('.info').locator('p').first();
+
+  await expect(editButton).toBeVisible();
+  await expect(groupTitle).toHaveText(editedIssuerName);
+  await expect(description).toHaveText(editedIssuerDesc);
+});
+})
