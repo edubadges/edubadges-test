2433da04d59818a0ef76caf7acf2ce9258ce4e57
Max van Pelt <55793121+ikbenwasmachine@users.noreply.github.com>
2025-05-15 14:23:36 +0200

    Initial end to end tests
    full: create all needed resources to award a badge, and deny it
    partial: Use existing resources to award a badge, reject it, reaccept it, and revoke it

---
+++ tests/E2E/Full/EndToEnd.spec.ts
@@ -0,0 +1,78 @@
+import { expect, test } from '../../../fixtures/staffFixture';
+
+test(`End to end testing by creating every resource needed`, 
+    { tag: '@E2E' }, 
+    async ({ 
+        adminPage, 
+        catalogPage,
+        extraStaffLoginPage,
+    }) => {
+    // var
+    const issuergroup = adminPage.managePage.issuerGroupPage;
+    const issuers = adminPage.managePage.issuersPage;
+    const userManagement = adminPage.managePage.userManagement;
+
+    const institution = 'WO';
+    const issuergroupName = 'E2E Issuergroup';
+    const descriptionText = 'E2E filler description';
+    const issuerName = 'E2E Issuer';
+    const badgeName = 'E2E Regular badge';
+    const badgeLocator = catalogPage.page
+      .locator('.card.badge')
+      .getByText(badgeName)
+      .first()
+      .locator('../../..');
+
+    const studentInfo = await adminPage.getStudentAccount(institution);
+    const institutionServer =
+        await userManagement.getInstitutionServer(institution);
+    const newBadgeAdminName = 'NewE2EBadgeAdmin';
+    const newBadgeAdminMail = newBadgeAdminName + '@' + institutionServer;
+
+    // setup
+    await adminPage.loginTestIdp('WO', 'Institution');
+
+    // create issuergroup
+    await adminPage.goToManage();
+    await adminPage.managePage.goToIssuerGroups();
+    await issuergroup.addNewIssuerGroup(issuergroupName, descriptionText);
+
+    // create issuer
+    await issuers.createNewIssuer(issuerName, descriptionText);
+
+    // create badgeclass
+    await issuers.createRegularBadge(badgeName);
+
+    // invite and accept new badgeclass admin
+    await adminPage.managePage.userManagement.inviteUser(newBadgeAdminMail);
+    await extraStaffLoginPage.loginDummyIdp(
+        newBadgeAdminName,
+        newBadgeAdminMail,
+        institutionServer,
+    );
+    await extraStaffLoginPage.validateLoginSuccessful();
+
+    // award badge and validate
+    await extraStaffLoginPage.badgeClassPage.directAwardBadge(badgeName, studentInfo.email);
+    await catalogPage.page.goto('');
+    await catalogPage.page.getByRole('link', { name: 'Open your backpack' }).click();
+    await catalogPage.loginStudentIdp(institution);
+    await catalogPage.page.getByRole('link', { name: 'My backpack' }).click();
+    await expect(catalogPage.page.getByText(badgeName)).toBeVisible();
+
+    // reject badge and validate
+    await catalogPage.reloadPage();
+    await badgeLocator.getByText(
+        'View details to claim this edubadge')
+        .click();
+
+    await catalogPage.page.getByRole('link', { name: 'Reject' }).click();
+    await catalogPage.page.getByRole('link', { name: 'Confirm' }).click();
+
+    await catalogPage.waitForLoadingToStop();
+
+    await expect(catalogPage.page.getByText(
+      'Edubadge is rejected'
+    )).toBeVisible();
+    await expect(badgeLocator).not.toBeVisible();
+});
---
+++ tests/E2E/Partial/EndToEnd.spec.ts
@@ -0,0 +1,57 @@
+import { expect, test } from '../../../fixtures/studentFixture';
+
+test(`End to end testing with existing resources`, 
+    { tag: '@E2E' }, 
+    async ({
+        catalogPage,
+        backpackPage,
+        adminPage,
+    }) => {
+    // var
+    const institution = 'WO';
+    const badgeName = 'Circulation and Breathing';
+
+    const reason = 'Reason for revocation is valid';
+    const studentInfo = await backpackPage.getStudentAccount('WO');
+    const badgeLocator = await backpackPage.getBadgeLocator(badgeName);
+
+    // setup
+    await backpackPage.login(institution);
+    await expect(badgeLocator).not.toBeVisible();
+    await adminPage.loginTestIdp('WO', 'Institution');
+
+    // request badge
+    await catalogPage.searchWithText(badgeName);
+    await catalogPage.filterOn(institution);
+    await catalogPage.openBadge(badgeName);
+    await catalogPage.requestEdubadge(institution);
+
+    // award badge and validate
+    await expect(badgeLocator).not.toBeVisible();
+    await adminPage.badgeClassPage.approveRequest(badgeName, studentInfo.email);
+    await backpackPage.reloadPage();
+    await backpackPage.openBackpack();
+    await expect(badgeLocator).toBeVisible();
+
+    // remove badge and validate
+    await backpackPage.rejectReceivedBadge(badgeName);
+    await expect(backpackPage.page.getByText(
+            'This edubadge has been removed. You can no longer download or share this edubadge')
+            .or(backpackPage.page.getByText(
+                'Edubadge is rejected'))
+            ).toBeVisible();
+
+    // revoke and validate
+    await adminPage.goToBadgeClasses();
+    await adminPage.badgeClassPage.searchWithText(badgeName);
+    await adminPage.badgeClassPage.openBadge(badgeName);
+    await adminPage.badgeClassPage.revokeAwardedBadge(studentInfo.name, reason);
+
+    await catalogPage.reloadPage();
+    await catalogPage.page.getByRole('link', { name: 'My backpack' }).click();
+    await expect(catalogPage.page.getByText(badgeName)).not.toBeVisible();
+    await catalogPage.page.getByRole('link', { name: 'Archive' }).click();
+    await expect(catalogPage.page.getByText(badgeName)).toBeVisible();
+    await catalogPage.page.getByText(badgeName).click();
+    await expect(catalogPage.page.getByText(reason)).toBeVisible();
+});
